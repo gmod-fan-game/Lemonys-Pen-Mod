26ee760707c62f96f96804e86f5b4fc71c8a8cef
gmod-fan-game <carter.aaron.cope+1@gmail.com>
2025-02-05 15:13:22 -0700

    Add files via upload

---
+++ Lemonys Pen Mod/Lemonys Pen Mod/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Lemonys_Pen_Mod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.lemony.gorillatag.lemonys pen mod";
+        public const string Name = "Lemonys_Pen_Mod";
+        public const string Version = "1.0.0";
+    }
+}
